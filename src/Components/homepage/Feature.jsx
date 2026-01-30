d74e1ec1bee78dd8a6dfe83390a6e9e80287e86b
yuossef151 <yuossefgaafar07@gmail.com>
2026-01-31 01:59:42 +0200

    Add slider component

---
+++ src/Components/homepage/Feature.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Feature() {
+  return (
+    <>
+      <div className='flex py-30 pe-24.5 ps-15 gap-15.5 bg-[#F5F5F5]'>
+        <div className=''>
+            <img src="./public/img-1.png" alt="" />
+        <h2 className='pt-4 pb-2 text-[18px] font-bold'>Fast & Reliable Shipping</h2>
+        <p className=' text-[16px] font-normal text-[#22222280]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo.</p>
+        </div>
+        <div>
+            <img src="./public/img-2.png" alt="" />
+        <h2 className='pt-4 pb-2 text-[18px] font-bold'>Secure Payment</h2>
+        <p className=' text-[16px] font-normal text-[#22222280]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo.</p>
+        </div>
+        <div>
+            <img src="./public/img-3.png" alt="" />
+        <h2 className='pt-4 pb-2 text-[18px] font-bold'>Easy Returns</h2>
+        <p className=' text-[16px] font-normal text-[#22222280]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo.</p>
+        </div>
+        <div>
+            <img src="./public/img-4.png" alt="" />
+        <h2 className='pt-4 pb-2 text-[18px] font-bold'>24/7 Customer Support</h2>
+        <p className=' text-[16px] font-normal text-[#22222280]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et ultricies est. Aliquam in justo varius, sagittis neque ut, malesuada leo.</p>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Img from '../../Components/homepage/img'
+import Feature from '../../Components/homepage/Feature'
 
 export default function Home() {
   return (
     <>
       <Img />
+      <Feature />
     </>
   )
 }
